758ec63ce949652b467d1dd7622b1f7ac4d4f54a
alexanderzhirnov <zhirnovlexanderoffyzhirnov@gmail.com>
2025-09-06 21:45:59 +0400

    Мобилка

--- Medkhan/settings.py
+++ Medkhan/settings.py
@@ -138,7 +138,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Для collectstatic
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.ngrok-free.app",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
